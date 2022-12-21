5a5a9419ce58260c8fc5ab5f1070f1c6ccc73ab5
mikedev <mik3dev@gmail.com>
2022-12-21 16:43:36 +0100

    update anaconda project and paths

--- utils/paths.py
+++ utils/paths.py
@@ -1,10 +1,9 @@
 from path import Path
 
-CODE_ROOT: Path = Path(__file__).parent.parent
-CODE_MODEL: Path = CODE_ROOT / 'model'
-ROOT: Path = CODE_ROOT.parent
-CONFIG: Path = CODE_ROOT / 'config'
-DATA: Path = ROOT / 'data'
+ROOT: Path = Path(__file__).parent.parent
+CODE_MODEL: Path = ROOT / 'model'
+CONFIG: Path = ROOT / 'config'
+DATA: Path = ROOT / 'data_storage'
 CONFIG_DATA: Path = CONFIG / 'dataset'
 CONFIG_MODEL: Path = CONFIG / 'model'
 CONFIG_MODEL_DATASET: Path = CONFIG / 'model_dataset'
