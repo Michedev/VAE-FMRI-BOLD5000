e435a32165a268c2aa08b02479b5818c4c5442d6
Michedev <mdv1994@gmail.com>
2022-12-24 22:11:13 +0000

    fixed and tested download data procedure

--- model/vae.py
+++ model/vae.py
@@ -3,6 +3,7 @@ import torch
 from torch import nn
 from torch.nn import functional as F
 
+
 class VAE(pl.LightningModule):
 
     def __init__(self, encoder: nn.Module, decoder: nn.Module, latent_size: int,
@@ -69,7 +70,6 @@ class VAEImgDecoder(pl.LightningModule):
         self._opt = opt
         self.latent_size = latent_size
 
-
     def forward(self, x):
         mu, sigma, dist, z = self.forward_encoder(x)
         x_hat = self.decoder(z)
@@ -87,7 +87,8 @@ class VAEImgDecoder(pl.LightningModule):
     def training_step(self, batch, batch_idx):
         x = batch['roi']
         result = self.forward(x)
-        loss = F.binary_cross_entropy_with_logits(target=x, input=result['x_hat'])
+        loss = F.binary_cross_entropy_with_logits(
+            target=x, input=result['x_hat'])
         if self.global_step % 100 == 0:
             self.log('loss/train_loss', loss, on_step=True, on_epoch=False,
                      prog_bar=True, logger=True)
@@ -96,7 +97,46 @@ class VAEImgDecoder(pl.LightningModule):
     def validation_step(self, batch, batch_idx):
         x = batch['roi']
         result = self.forward(x)
-        loss = F.binary_cross_entropy_with_logits(target=x, input=result['x_hat'])
+        loss = F.binary_cross_entropy_with_logits(
+            target=x, input=result['x_hat'])
         self.log('loss/valid_loss', loss, on_step=True, on_epoch=True,
                  prog_bar=True, logger=True)
         return loss
+
+    def configure_optimizers(self):
+        return self._opt(self.parameters())
+
+class ROIDecoder(pl.LightningModule):
+    """
+    Decode ROI vector get from all the brain areas to the viewed image
+    """
+
+    def __init__(self, decoder: nn.Module, opt: type[torch.optim.Optimizer] = torch.optim.Adam):
+        super().__init__()
+        self.decoder = decoder
+        self._opt = opt
+
+    def forward(self, z):
+        return self.decoder(z)
+    
+    def training_step(self, batch, batch_idx):
+        x = batch['roi']
+        result = self.forward(x)
+        loss = F.binary_cross_entropy_with_logits(
+            target=x, input=result['x_hat'])
+        if self.global_step % 100 == 0:
+            self.log('loss/train_loss', loss, on_step=True, on_epoch=False,
+                     prog_bar=True, logger=True)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x = batch['roi']
+        result = self.forward(x)
+        loss = F.binary_cross_entropy_with_logits(
+            target=x, input=result['x_hat'])
+        self.log('loss/valid_loss', loss, on_step=True, on_epoch=True,
+                 prog_bar=True, logger=True)
+        return loss
+
+    def configure_optimizers(self):
+        return self._opt(self.parameters())
